6d65fb496545912b14af50ac39554a9d04d96bef
Brent M. Spell <bspell1@gmail.com>
2022-07-12 16:26:21 -0400

    fix install

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="improved-diffusion",
-    py_modules=["improved_diffusion"],
+    packages=find_packages(),
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    license=("LICENSE"),
+    version="0.0.1",
 )
